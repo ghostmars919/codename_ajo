a170d0d243ac1aab5062fcba9a2ee5e0fa8703be
ghostmars919 <ghostmars919@gmail.com>
2014-12-06 17:58:41 +0100

    Update engine.php
    Aggiunta la funzione splitSentence per poter tradurre più parole contemporaneamente

--- engine/engine.php
+++ engine/engine.php
@@ -1,11 +1,12 @@
 <?php
 	include '../config/engine-config.php';
+	$sentence;
 	
 	/*Funzione che, prende il parametro passatogli e lo confronta con gli elementi di un database
 	trovato l elemento cercato restituisce la traduzione in sardo o viceversa.*/
 	function translate ($word)
 	{
-		$query = mysql_query("SELECT sardo FROM parole WHERE '$word' = italiano");
+		$query = mysql_query("SELECT sardo FROM parole WHERE '$word' = italiano") or die();
 		//$row = mysql_fetch_array($query);
 
 		while($row = mysql_fetch_array($query))
@@ -14,5 +15,19 @@
 		}
 	}
 
-	translate($_GET['input-parola']);
+	/*Funzione per la divisione delle frasi*/
+	function splitSentence($s){
+		global $sentence;
+
+		$split = explode(" ", $s);
+
+		foreach($split as $Split){
+
+			translate($Split);
+		}
+
+	}
+
+	//translate($_GET['input-parola']);
+	splitSentence($_GET['input-parola']);
 ?>
